2cb8c98c8c48043779acd3fd88fd204ff9982b55
Your Name <you@example.com>
2020-07-09 00:46:00 +0530

    name calulator

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 (function () {
 'use strict';
 
+angular.module('NameCalculator', [])
 
-	angular.module('myfirstapp',[])
-	.controller('myFirstConroller',function($scope)
-	{
-		$scope.name = "arpit";
-		$scope.sayhello = function(){
-			return "Hello Coursera"
-		}
-	});
+.controller('NameCalculatorController', function ($scope) {
+  $scope.name = "";
+  $scope.totalValue = 0;
+
+  $scope.displayNumeric = function () {
+    var totalNameValue = calculateNumericForString($scope.name);
+    $scope.totalValue = totalNameValue;
+  };
+
+
+  function calculateNumericForString(string) {
+    var totalStringValue = 0;
+    for (var i = 0; i < string.length; i++) {
+      totalStringValue += string.charCodeAt(i);
+    }
+
+    return totalStringValue;
+  }
+
+});
+
 
 })();
